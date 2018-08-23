1d4ab1be316e397e72d0a1863ec56cef35b9863a
Fernanda Velazquez <fvel4z@gmail.com>
2018-08-22 21:07:27 -0500

    added state to the search bar

--- src/App.js
+++ src/App.js
@@ -2,7 +2,25 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// Implement a fuzzy search algorithm for transactions with the following requirements:
+// Have a web page with one input field where a user can enter text, and a displayed list of transactions
+// Display the full list of transactions when the input field is empty
+// Update the displayed list of transactions as the user types in the input field (only show transactions that match the fuzzy search criteria)
+// Show the transactions in order by date
+// Fuzzy search must account for all transaction object properties (amount, date, card_last_four)
+// You can hardcode the transaction data found below and do not need to fetch it from a server
+// Unit tests are encouraged
+
 class App extends Component {
+
+  state = { query: '' }
+  
+  handleInputChange = () => {
+    this.setState({
+      query: this.search.value
+    })
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,21 +28,16 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Search Transactions</h1>
         </header>
+
+        <input className="searchInput" placeholder="Search for transactions..." ref={input => this.search = input} onChange={this.handleInputChange}/>
+        <p>{this.state.query}</p>
+
       </div>
 
     );
   }
-}
 
-// Implement a fuzzy search algorithm for transactions with the following requirements:
-// Have a web page with one input field where a user can enter text, and a displayed list of transactions
-// Display the full list of transactions when the input field is empty
-// Update the displayed list of transactions as the user types in the input field (only show transactions that match the fuzzy search criteria)
-// Show the transactions in order by date
-// Fuzzy search must account for all transaction object properties (amount, date, card_last_four)
-// You may use any front-end library (we currently use ReactJS) for the design/architecture of the application, but you may not use a library for the actual search algorithm
-// You can hardcode the transaction data found below and do not need to fetch it from a server
-// Unit tests are encouraged
+}
 
 const transactions = [
   { amount: 112.98, date: '27-01-2018T12:34', card_last_four: '2544' },
