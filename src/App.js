46674c23a7074226a6f30ac13d3ad649c6c45af5
Fernanda Velazquez <fvel4z@gmail.com>
2018-08-23 19:20:53 -0500

    search working

--- src/App.js
+++ src/App.js
@@ -1,55 +1,57 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-// Implement a fuzzy search algorithm for transactions with the following requirements:
-// Have a web page with one input field where a user can enter text, and a displayed list of transactions
-// Display the full list of transactions when the input field is empty
-// Update the displayed list of transactions as the user types in the input field (only show transactions that match the fuzzy search criteria)
-// Show the transactions in order by date
-// Fuzzy search must account for all transaction object properties (amount, date, card_last_four)
-// You can hardcode the transaction data found below and do not need to fetch it from a server
-// Unit tests are encouraged
+import {thetransactions} from './transactions'
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      query: '',
+    }
 
-  state = { query: '' }
-  
-  handleInputChange = () => {
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
     this.setState({
-      query: this.search.value
+      query: event.target.value
     })
   }
 
   render() {
+    let filteredTransactions = thetransactions.filter(
+      (trans) => {
+        return trans.amount.toString().indexOf(this.state.query) !== -1 || trans.date.indexOf(this.state.query) !== -1 || trans.card_last_four.indexOf(this.state.query) !== -1 
+      }
+    )
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Search Transactions</h1>
         </header>
+        
+        <form onSubmit={this.displayingTransactions}>
+          <input className="searchInput" 
+            placeholder="Search for transactions..." 
+            value={this.state.query} 
+            ref={input => this.search = input} 
+            onChange={this.handleChange}
+          />
+          <p>{this.state.query}</p>
+        </form>
 
-        <input className="searchInput" placeholder="Search for transactions..." ref={input => this.search = input} onChange={this.handleInputChange}/>
-        <p>{this.state.query}</p>
+        <div className="TransList">
+          <ul>{filteredTransactions.map((trans) => {
+            return <li>Amount: {trans.amount}, Date: {trans.date}, Last four numbers: {trans.card_last_four}</li>
+          })}</ul>
+        </div>
 
       </div>
-
     );
   }
-
 }
-
-const transactions = [
-  { amount: 112.98, date: '27-01-2018T12:34', card_last_four: '2544' },
-  { amount: 0.45, date: '01-12-2017T9:36', card_last_four: '4434' },
-  { amount: 95.99, date: '23-11-2017T14:34', card_last_four: '3011' },
-  { amount: 7774.32, date: '17-07-2017T03:34', card_last_four: '6051' },
-  { amount: 1345.98, date: '22-06-2017T10:33', card_last_four: '0059' },
-  { amount: 2850.70, date: '27-01-2018T12:34', card_last_four: '4444' },
-  { amount: 45.00, date: '10-02-2018T02:34', card_last_four: '0110' },
-  { amount: 1.00, date: '17-02-2018T18:34', card_last_four: '1669' },
-  { amount: 4.69, date: '01-02-2018T02:34', card_last_four: '8488' },
-  { amount: 1111.11, date: '15-01-2018T21:34', card_last_four: '9912' }
-];
 
 export default App;
---
+++ src/transactions.js
@@ -0,0 +1,28 @@
+export const thetransactions = [
+    { amount: 112.98, date: '27-01-2018T12:34', card_last_four: '2544' },
+    { amount: 0.45, date: '01-12-2017T9:36', card_last_four: '4434' },
+    { amount: 95.99, date: '23-11-2017T14:34', card_last_four: '3011' },
+    { amount: 7774.32, date: '17-07-2017T03:34', card_last_four: '6051' },
+    { amount: 1345.98, date: '22-06-2017T10:33', card_last_four: '0059' },
+    { amount: 2850.70, date: '27-01-2018T12:34', card_last_four: '4444' },
+    { amount: 45.00, date: '10-02-2018T02:34', card_last_four: '0110' },
+    { amount: 1.00, date: '17-02-2018T18:34', card_last_four: '1669' },
+    { amount: 4.69, date: '01-02-2018T02:34', card_last_four: '8488' },
+    { amount: 1111.11, date: '15-01-2018T21:34', card_last_four: '9912' }
+];
+
+thetransactions.forEach((i) => {
+    let day = i.date.slice(0, 2)
+    let month = i.date.slice(3, 5)
+    let year = i.date.slice(6, 10)
+    let miF = month + "-" + day + "-" + year;
+    miF = new Date(miF).getTime()
+    i['date2'] = miF
+    let finalDate = day + "-" + month + "-" + year;
+    i['date'] = finalDate
+    return i;
+});
+
+thetransactions.sort(function(a, b){
+  return a.date2 - b.date2
+})
